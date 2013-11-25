2d20d790691a2b4af74e5af2fe0b1aee975f6cef
Sean C Davis <scdavis41@gmail.com>
2013-11-25 12:29:59 -0500

    add buttons doc and convert exmaple to php

---
+++ examples/partials/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Bones Examples</title>
+    <link rel="stylesheet" href="../assets/stylesheets/bones.css">
+  </head>  
+  <body>
