41044cf15f93cf02bc80933772aaac4add3abc04
Sean C Davis <scdavis41@gmail.com>
2013-12-03 12:19:24 -0500

    convert examples to html

--- examples/partials/header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Bones Examples</title>
-    <link rel="stylesheet" href="../assets/stylesheets/bones.css">
-  </head>  
-  <body>
